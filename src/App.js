066a2405adadcb517669481344cbc14e0459aeaa
Pratikkakkad <pratikkakkad21@gmail.com>
2020-03-21 16:01:21 +0530

    explore some read write methods

--- src/App.js
+++ src/App.js
@@ -1,36 +1,40 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import {database} from './firebase'
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import { database } from "./firebase";
+import "./App.css";
+import "antd/dist/antd.css";
 
 export default class App extends Component {
-  constructor(props){
-    super(props)
-    this.state={data: null}
+  constructor(props) {
+    super(props);
+    this.state = { data: null };
+
+    this.dataRef = null
   }
 
-  componentDidMount(){
-    
-    database.ref().on('value',(snapshot) => {
-      console.log("new dataa",snapshot)
-    })
+  componentDidMount() {
+    this.dataRef = database.ref('/users')
+    this.dataRef.on("value", snapshot => {
+      console.log("new dataa", snapshot.val());
+    });
+
+    this.dataRef.push({ name: "Pratikk" });
   }
 
   render() {
     return (
-
       <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>        
-      </header>
-      <div>
-        <h3>Firebase data will come here...</h3>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+        </header>
+
+        <div>
+          <h3>Firebase data will come here...</h3>
+        </div>
       </div>
-    </div>
-    )
+    );
   }
 }
-
