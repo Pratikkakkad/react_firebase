afcbbe8da9fb6b14774912e35f0ac405c5edcee7
Amol T <amoltangade@dbatu.ac.in>
2020-03-21 13:51:51 +0530

    setup firebase

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
+import {database} from './firebase'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+export default class App extends Component {
+  constructor(props){
+    super(props)
+    this.state={data: null}
+  }
+
+  componentDidMount(){
+    
+    database.ref().on('value',(snapshot) => {
+      console.log("new dataa",snapshot)
+    })
+  }
+
+  render() {
+    return (
+
+      <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        </p>        
       </header>
+      <div>
+        <h3>Firebase data will come here...</h3>
+      </div>
     </div>
-  );
+    )
+  }
 }
 
-export default App;
