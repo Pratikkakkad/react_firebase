6c62c2cf19a6230d3c3febc9ccbca7ffb9ca96e3
Amol T <amoltangade@dbatu.ac.in>
2020-03-22 10:15:02 +0530

    setup auth

--- src/App.js
+++ src/App.js
@@ -4,31 +4,28 @@ import { database } from "./firebase";
 import "./App.css";
 import "antd/dist/antd.css";
 import Main from "./components/Main";
+import HeaderMenu from "./components/Header";
 
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = { data: null };
-
     this.dataRef = null;
   }
 
-  componentDidMount() {
-    this.dataRef = database.ref("/users");
-    this.dataRef.on("value", snapshot => {
-      console.log("new dataa", snapshot.val());
-    });
+  // componentDidMount() {
+  //   this.dataRef = database.ref("/users");
+  //   this.dataRef.on("value", snapshot => {
+  //     console.log("new dataa", snapshot.val());
+  //   });
 
-    this.dataRef.push({ name: "Pratikk" });
-  }
+  //   this.dataRef.push({ name: "Pratikk" });
+  // }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <Main />
-        </header>
+            <HeaderMenu/>
 
         <div>
           <h3>Firebase data will come here...</h3>
